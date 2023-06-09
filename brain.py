07c0bf4bb407a258bbaecb758d73538695810d52
sahis2002 <89941586+SahisN@users.noreply.github.com>
2023-06-08 17:46:27 -0700

    adding starting project

---
+++ brain.py
@@ -0,0 +1,17 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsInceptionV3()
+prediction.setModelPath(
+    os.path.join(execution_path, "./reallysmartbrain/inception_v3_google-1a9a5a14.pth")
+)
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(
+    os.path.join(execution_path, "./reallysmartbrain/giraffe.jpg"), result_count=5
+)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, " : ", eachProbability)
